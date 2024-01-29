1b7b5455b6ec7cfdb37d2536a25a86ecd7219082
chinesesunny <chinesecpp@aliyun.com>
2024-01-29 19:48:49 +0800

    b.cpp

---
+++ b.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: b.cpp
+	> Author: Sunny
+	> Mail:chinesecpp@aliyun.com 
+	> Created Time: Mon 29 Jan 2024 07:48:30 PM +08
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
